84bc73f3ed43813fba1a80b631dc1248cbf454f6
Alumnos <Alumnos@192.168.3.150>
2024-03-18 08:26:31 -0300

    Ejercicio24

---
+++ GuiadeRepaso/src/guia/Ejercicio24.java
@@ -0,0 +1,18 @@
+package guia;
+
+public class Ejercicio24 {
+	
+	//Escribir una función que reciba un arreglo de enteros 
+	//y devuelva true si el arreglo está ordenado 
+	//de mayor a menor y false
+	
+	public static boolean estaOrdenado (int [] v) {
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		int [] vectorito = {1,5,3,6,4,2,7,8,9};
+	}
+
+}
