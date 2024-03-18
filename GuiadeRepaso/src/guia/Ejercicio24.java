ddba65d240ccb0307b05a736273d12a692f5b449
Alumnos <Alumnos@192.168.3.150>
2024-03-18 08:59:55 -0300

    Ej24

--- GuiadeRepaso/src/guia/Ejercicio24.java
+++ GuiadeRepaso/src/guia/Ejercicio24.java
@@ -6,13 +6,31 @@ public class Ejercicio24 {
 	//y devuelva true si el arreglo está ordenado 
 	//de mayor a menor y false
 	
+//	public static boolean estaOrdenado (int [] v) {
+//		for (int i = 0 ; i < v.length - 1;i++) {
+//			if (v[i] < v[i+1]) {
+//				return false;
+//			}
+//			
+//		}
+//		return true;
+//	}
 	public static boolean estaOrdenado (int [] v) {
-		
+		boolean ordenado = true;
+		int i = 0;
+		while (i < v.length -1 && ordenado)  {
+			ordenado = v[i] > v[i+1];
+			i++;
+		}
+		return ordenado;
 	}
+	
 	
 	public static void main(String[] args) {
 		
-		int [] vectorito = {1,5,3,6,4,2,7,8,9};
+		int [] vectorito = {9,7,5,4,3,2,1};
+		estaOrdenado(vectorito);
+		
 	}
 
 }
